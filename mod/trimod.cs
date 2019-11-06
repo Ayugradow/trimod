5b69648991068cbede3c53a72f876534cfda8325
Ayugradow <32333973+Ayugradow@users.noreply.github.com>
2019-11-05 23:44:05 -0300

    Adding more files I guess

---
+++ mod/trimod.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using Modding;
+using Reflection = Modding.ReflectionHelper;
+using UnityEngine.SceneManagement;
+
+
+namespace TriMod
+{
+	public class TriMod : Mod
+	{
+
+		public override void Initialize()
+		{
+			LogDebug("Bonfire Mod initializing!");
+
+			ModHooks.Instance.NewGameHook += SetupGameRefs;
+			ModHooks.Instance.SavegameLoadHook += SetupGameRefs;
+//			UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
+			LogDebug("Bonfire Mod v." + GetVersion() + " initialized!");
+		}
+
+		private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+		{
+			if (GameManager.instance.IsGameplayScene() && UIManager.instance.uiState.ToString() == "PLAYING")
+			{
+				if (hc == null && HeroController.instance != null)
+				{
+					hc = HeroController.instance;
+					if (spriteFlash == null)
+					{
+						spriteFlash = hc.GetComponent<SpriteFlash>();
+						LogDebug("Hero object set. SpriteFlash component gotten.");
+					}
+				}
+			}
+
+			if (pd == null && PlayerData.instance != null)
+				pd = PlayerData.instance;
+		}
+
+        
+
+		public void SetupGameRefs()
+		{
+			if (gm == null)
+			{
+				gm = GameManager.instance;
+			}
+			
+			if (pd == null && PlayerData.instance != null)
+				pd = PlayerData.instance;
+			
+			if (GameManager.instance.IsGameplayScene() && UIManager.instance.uiState.ToString() == "PLAYING")
+			{
+				if (hc == null && HeroController.instance != null)
+				{
+					hc = HeroController.instance;
+					if (spriteFlash == null)
+					{
+						spriteFlash = hc.GetComponent<SpriteFlash>();
+						LogDebug("Hero object set. SpriteFlash component gotten.");
+					}
+				}
+			}
+
+			gm.gameObject.AddComponent<Berserker>();
+		}
+
+		public void SetupGameRefs(int id)
+		{
+			SetupGameRefs();
+		}
+        
+		public static GameManager gm;
+		public static PlayerData pd;
+		public static HeroController hc;
+		public static SpriteFlash spriteFlash;
+		public int voidCount = 0;
+		public bool isBerserk = false;
+	}
+}
